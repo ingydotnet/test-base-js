61196825a8df973cc88c6e8327a955faf0cd1b4a
ingy <ingy@139b8d9b-c9ab-42d9-a619-4cf020c53d71>
2008-08-21 22:27:29 +0000

    Add a newSubclass method to Test.Base.

--- sample/lib/Test/Sample.js
+++ sample/lib/Test/Sample.js
@@ -1,42 +1,11 @@
-// Class Test.Sample
-(function(className) {
-var __ = (Test.Sample = function() {
-    this.init.apply(this, arguments);
-    this.className = className;
-}).prototype = new Test.Base();
+(function() {
 
-__.init = function() {
-    Test.Base.prototype.init.call(this);
-    this.block_class = 'Test.Sample.Block';
-}
+Test.Base.newSubclass('Test.Sample');
 
-})('Test.Sample');
+var proto = Test.Sample.Filter.prototype;
 
-
-// Class Test.Sample.Block
-(function(className) {
-var __ = (Test.Sample.Block = function() {
-    this.init.apply(this, arguments)
-    this.className = className;
-}).prototype = new Test.Base.Block();
-
-__.init = function() {
-    Test.Base.Block.prototype.init.call(this);
-    this.filter_object = new Test.Sample.Filter();
-}
-
-})('Test.Sample.Block');
-
-
-// Class Test.Sample.Filter
-(function(className) {
-var __ = (Test.Sample.Filter = function() {
-    this.init.apply(this, arguments);
-    this.className = className;
-}).prototype = new Test.Base.Filter();
-
-__.upper_case = function(content, block) {
+proto.upper_case = function(content, block) {
     return content.toUpperCase();
 }
 
-})('Test.Sample.Filter');
+})();
